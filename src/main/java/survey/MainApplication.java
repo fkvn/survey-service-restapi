01f89699baa50631a9af2f778de6614f9087923f
fkvn <phucaone@gmail.com>
2020-01-30 08:52:54 -0800

    enabled CORS access
    enabled globally in Spring Boot Application (MainApplication.java)

--- src/main/java/survey/MainApplication.java
+++ src/main/java/survey/MainApplication.java
@@ -2,6 +2,9 @@ package survey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MainApplication {
@@ -9,5 +12,15 @@ public class MainApplication {
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
   }
+  
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
 }
